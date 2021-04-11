9d7e698fe8c721b84f4951c607936b2c0dec1320
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2021-04-11 21:28:45 +0200

    Update environment

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-jupyter-lab \
-  --ip=0.0.0.0
-  --port=8888 \
-  --ContentsManager.allow_hidden=True \
-  --ServerApp.password $JUPYTER_PASSWORD_HASH \
-  --notebook-dir=/workspace \
-  --no-browser \
-  --allow-root
